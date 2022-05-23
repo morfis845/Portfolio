08f3ddb44bdabfada8865e907fd2fc60dd1e6739
morfis845 <manciajose1208@gmail.com>
2022-05-23 16:06:46 -0600

    Desktop version and snake  game

--- public/build/js/app.js
+++ public/build/js/app.js
@@ -1,1 +1,1 @@
-const openMenu=document.querySelector(".open-menu"),closeMenu=document.querySelector(".close-menu");openMenu.addEventListener("click",(function(){const e=document.querySelector(".nav");e.classList.remove("hide"),e.classList.add("show")})),closeMenu.addEventListener("click",(function(){const e=document.querySelector(".nav");e.classList.remove("show"),e.classList.add("hide")}));
+const hamburger=document.querySelector(".hamburger");hamburger.addEventListener("click",(function(){document.querySelector(".nav").classList.toggle("show"),hamburger.classList.toggle("close")}));
